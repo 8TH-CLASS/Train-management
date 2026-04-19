30d794c11112a03704f38cbc56a00885a8a14999
HARSH RAY <harshroy2637@gmail.com>
2026-04-19 17:14:13 +0530

    FINAL

--- src/ticket.java
+++ src/ticket.java
@@ -8,25 +8,26 @@ public class ticket {
     private int seatBooked ;
     private static int counter = 1001;
 
-
     //in future add price of ticket to
-
-    public ticket(int tickeid, User user, Train train, int seatBooked) {
+    public ticket(User user, Train train, int seatBooked) {
         this.tickeid = counter++;
         this.user = user;
         this.train = train;
         this.seatBooked = seatBooked;
     }
 
+
     public int getTickeid() {
         return tickeid;
     }
 
     public void setTickeid(int tickeid) {
+
         this.tickeid = tickeid;
     }
 
     public User getUser() {
+
         return user;
     }
 
@@ -64,4 +65,6 @@ public class ticket {
         return "TICKET ID;" + tickeid + "|Train " + train.getName() + "|Route " +train.getSource() + " -> " + train.getDestination() +
                 "| Seats " +seatBooked+ "| Booked BY:" +user.getFullname();
     }
+
+
 }
